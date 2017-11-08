ba66c57cf770d1d009dc43edb1b62230a9f0b628
KwFung7 <lingfung09@hotmail.com>
2017-11-09 01:35:25 +0800

    [Particles] Implement particles.js

--- src/containers/portfolio_page.js
+++ src/containers/portfolio_page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PortfolioHeader from './portfolio_header'
 import PortfolioNav from './portfolio_nav'
-import store from './portfolio_store'
+import store from './portfolio_data'
+import ParticlesContainer from './particles_container'
 
 const PortfolioPage = props => {
   return (
     <div className = "portfolio-page">
+      <ParticlesContainer />
       <PortfolioHeader user = {store.header} />
       <PortfolioNav 
         intro = {store.intro}
