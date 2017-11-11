c08401944a3c31dfc1e3cc3e8da7b85148628a7d
KwFung7 <lingfung09@hotmail.com>
2017-11-12 00:06:56 +0800

    [Router] Implement react-router-rom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import PortfolioPage from './containers/portfolio_page'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.scss'
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <PortfolioPage />
+        <Router>
+          <div>
+            <Route exact path='/' component={PortfolioPage} />
+            <Route path='/portfolio' component={PortfolioPage} />
+          </div>
+        </Router>
       </MuiThemeProvider>
     )
   }
