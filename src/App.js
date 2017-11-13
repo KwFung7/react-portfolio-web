b22eb33429eb9367d57770e0aa9c02e7311b9948
KwFung7 <lingfung09@hotmail.com>
2017-11-14 01:30:08 +0800

    [No chatbot] no need chatbot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PortfolioPage from './containers/portfolio_page'
-import ChatbotPage from './containers/chatbot_page'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.scss'
@@ -13,7 +12,6 @@ class App extends Component {
           <div>
             <Route exact path='/' component={PortfolioPage} />
             <Route path='/portfolio' component={PortfolioPage} />
-            <Route path='/chatbot' component={ChatbotPage} />
           </div>
         </Router>
       </MuiThemeProvider>
