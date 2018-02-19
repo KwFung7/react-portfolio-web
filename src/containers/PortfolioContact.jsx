fb9c04a97ce3cfad6d82e5c39b6672469bff640e
KwFung7 <lingfung09@hotmail.com>
2018-02-19 21:57:49 +0800

    [SocialIcon] add social icon in contact

--- src/containers/PortfolioContact.jsx
+++ src/containers/PortfolioContact.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 import { EMAIL, TELEPHONE } from '../constants'
 
 class PortfolioContact extends React.Component {
 
   render() {
     const { contact = {} } = this.props
-    const { email = '', phone = '' } = contact
+    const { email = '', phone = '', github_link, linkedin_link } = contact
     return (
       <div className = "portfolio-contact">
         <div className = "portfolio-contact-email">{EMAIL} {email}</div>
         <div className = "portfolio-contact-phone">{TELEPHONE} {phone}</div>
+        <div className = "portfolio-contact-social-btn">
+          {
+            !_.isEmpty(github_link) &&
+            <a className="m-2" href={github_link} alt="github-btn" target="_blank">
+              <i className="fab fa-github"></i>
+            </a>
+          }
+          {
+            !_.isEmpty(linkedin_link) &&
+            <a className="m-2" href={linkedin_link} alt="linkedin-btn" target="_blank">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          }
+        </div>
       </div>
     )
   }
